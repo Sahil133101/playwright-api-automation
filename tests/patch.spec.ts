74b2d6565449e664cd5ad92ae820c2a2bcfefe56
Sahil Sharma <sahilsharmanakur@gmail.com>
2026-09-11 22:39:33 +0530

    update

--- tests/patch.spec.ts
+++ tests/patch.spec.ts
@@ -1,27 +1,36 @@
 import { test, expect } from "../fixtures/api-fixture";
+import bookingData from "../testData/post.json";
 
-test.describe("PATCH method", () => {
+test("Update firstname of booking", async ({ request, authToken }) => {
 
-    test("Update firstname of booking", async ({ request, authToken }) => {
-
-        const response = await request.patch("/booking/5", {
-
-            headers: {
-                Cookie: `token=${authToken}`
-            },
-
-            data: {
-                firstname: "Sahil"
-            }
-        });
-
-        expect(response.status()).toBe(200);
-
-        const body = await response.json();
-
-        console.log("PATCH Response:", body);
-
-        expect(body.firstname).toBe("Sahil");
+    // Create booking
+    const createResponse = await request.post("/booking", {
+        headers: {
+            Cookie: `token=${authToken}`
+        },
+        data: bookingData
     });
 
+    expect(createResponse.status()).toBe(200);
+
+    const createBody = await createResponse.json();
+    const bookingId = createBody.bookingid;
+
+    // PATCH created booking
+    const response = await request.patch(`/booking/${bookingId}`, {
+        headers: {
+            Cookie: `token=${authToken}`
+        },
+        data: {
+            firstname: "Sahil"
+        }
+    });
+
+    expect(response.status()).toBe(200);
+
+    const body = await response.json();
+
+    console.log("PATCH Response:", body);
+
+    expect(body.firstname).toBe("Sahil");
 });
