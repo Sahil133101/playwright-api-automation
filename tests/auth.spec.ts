f0f246d4d7531e6147f12eaef24d9e35c43fc4c8
Sahil Sharma <sahilsharmanakur@gmail.com>
2026-09-11 23:08:29 +0530

    Update API authentication test

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -5,16 +5,16 @@ dotenv.config({ override: true });
 
 test("Create auth token", async ({ request }) => {
 
-    console.log("BASE_URL:", process.env.BASE_URL);
-    console.log("USERNAME:", process.env.USERNAME);
-    console.log("PASSWORD:", process.env.PASSWORD);
+    console.log("BASE_URL:", process.env.API_BASE_URL);
+    console.log("USERNAME:", process.env.API_USERNAME);
+    console.log("PASSWORD:", process.env.API_PASSWORD);
 
     const response = await request.post(
         `${process.env.BASE_URL}/auth`,
         {
             data: {
-                username: process.env.USERNAME,
-                password: process.env.PASSWORD
+                username: process.env.API_USERNAME,
+                password: process.env.API_PASSWORD
             }
         }
     );
